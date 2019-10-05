a705a8773b40f8ee78ee91ef87094c6381a3adea
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-05 16:47:26 -0400

    modified:   make.sums.sh 	renamed:    md5.sum -> md5sum.sum 	new file:   sha1sum.sum 	renamed:    sha256.sum -> sha256sum.sum 	renamed:    sha512.sum -> sha512sum.sum

--- make.sums.sh
+++ make.sums.sh
@@ -1,18 +1,18 @@
 #!/bin/env sh 
 # Copyright 2019 (c) all rights reserved 
-# by BuildAPKs https://buildapks.github.io
+# by SDRausty https://sdrausty.github.io
 #####################################################################
-set -e -u
-
-md5sum * > md5.sum
-sha256sum * > sha256.sum
-sha512sum * > sha512.sum
-sed -i '/[0-9].sum/d' *.sum
-printf "\\nChecking md5sums...\\n"
-md5sum -c md5.sum
-printf "\\nChecking sha256sum...\\n"
-sha256sum -c sha256.sum
-printf "\\nChecking sha512sum...\\n"
-sha512sum -c sha512.sum
-
+for i in md5sum sha1sum sha256sum sha512sum 
+do
+	printf "\\nCreating $i...\\n"
+	$i * > $i.sum
+	sleep 0.1
+done
+sed -i '/[0-9]sum.sum/d' *.sum
+for i in md5sum sha1sum sha256sum sha512sum 
+do
+	printf "\\nChecking $i...\\n"
+	$i -c $i.sum
+	sleep 0.1
+done
 # make.sums.sh EOF
